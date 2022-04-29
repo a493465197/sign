3623f72732fad111c86f06bd853b3d7a65189795
李哲宇 <lzhey@servyou.com.cn>
2022-04-29 18:18:37 +0800

    fix

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 const Controller = require('egg').Controller;
 const md5 = require('md5')
-const apktool = require('../tools/node_apktool')
+// const apktool = require('../tools/node_apktool')
 // const puppeteer = require('puppeteer');
 class HomeController extends Controller {
   isPhone(ctx) {
@@ -27,13 +27,13 @@ class HomeController extends Controller {
       ctx
     } = this;
 
-    apktool.resigned_apk(path.resolve(__dirname, "../public/unsign.apk"), path.resolve(__dirname, "../public/sign.apk"), "~android/keystore", "123456", "lzy", function (err, result) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(result);
-    });
+    // apktool.resigned_apk(path.resolve(__dirname, "../public/unsign.apk"), path.resolve(__dirname, "../public/sign.apk"), "~android/keystore", "123456", "lzy", function (err, result) {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log(result);
+    // });
 
     ctx.body = 123
   }
